6533a4330553c485a6213313c99b4d73842e5699
Ed Chapman - Turing <edchapmanelc@gmail.com>
2023-07-19 16:29:10 +0100

    formatting only

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -1,15 +1,16 @@
-use std::{fs, path::Path};
-use std::collections::{HashSet, HashMap};
 use rand::prelude::Distribution;
-use rand::{thread_rng, Rng, distributions::WeightedIndex};
+use rand::{distributions::WeightedIndex, thread_rng, Rng};
+use std::collections::{HashMap, HashSet};
+use std::{fs, path::Path};
 
 use transformer::model::Transformer;
 fn main() {
-
     // load in data and prepare
     let txt = fs::read_to_string(
-                Path::new(std::env::var("CARGO_WORKSPACE_DIR").unwrap().as_str())
-                .join("data/tiny_shakespeare.txt")).unwrap();
+        Path::new(std::env::var("CARGO_WORKSPACE_DIR").unwrap().as_str())
+            .join("data/tiny_shakespeare.txt"),
+    )
+    .unwrap();
 
     let all_chars: Vec<char> = (&txt).chars().collect();
 
@@ -18,13 +19,13 @@ fn main() {
     for c in (&all_chars).into_iter() {
         set.insert(*c);
     }
-    let mut chars:Vec<char> = Vec::from_iter(set);
+    let mut chars: Vec<char> = Vec::from_iter(set);
     chars.sort_by(|a, b| b.cmp(a));
     chars.reverse();
     // println!("{:?}",chars);
     let mut chartoi = HashMap::new();
     let mut itochar = HashMap::new();
-    for (i,c) in chars.iter().enumerate() {
+    for (i, c) in chars.iter().enumerate() {
         chartoi.insert(*c, i);
         itochar.insert(i, *c);
     }
@@ -43,9 +44,13 @@ fn main() {
     let batch_size = 1;
     let head_size = n_embd; // for now, without ff-nn's to project head_size -> n_embd
 
-    fn get_batch(data:Vec<usize>, batch_size: usize, block_size: usize) -> (Vec<Vec<usize>>,Vec<Vec<usize>>) {
+    fn get_batch(
+        data: Vec<usize>,
+        batch_size: usize,
+        block_size: usize,
+    ) -> (Vec<Vec<usize>>, Vec<Vec<usize>>) {
         // each x sample in the batch contains block_size (eg = 4) items eg. [x1,x2,x3,x4]
-        // and that sample contains 4 training samples or different length, 
+        // and that sample contains 4 training samples or different length,
         // each corresponding to the 4 y samples returned with it:
         // x1 -> y1(=x2)
         // x1,x2 -> y2(=x3)
@@ -56,36 +61,42 @@ fn main() {
         let mut x = Vec::new();
         let mut y = Vec::new();
         for _ in 0..batch_size {
-            let idx = rng.gen_range(0..(data.len()-block_size));
-            x.push(data[idx..(idx+block_size)].to_vec());
-            y.push(data[idx+1..(idx+block_size+1)].to_vec())
+            let idx = rng.gen_range(0..(data.len() - block_size));
+            x.push(data[idx..(idx + block_size)].to_vec());
+            y.push(data[idx + 1..(idx + block_size + 1)].to_vec())
         }
-        (x,y)
+        (x, y)
     }
 
-    println!("{:?}",chars);
+    // println!("{:?}", chars);
 
-    let (x_batch,y_batch) = get_batch(charidxs, batch_size, block_size);
-    println!("x = {:?}",x_batch);
-    println!("y = {:?}",y_batch);
-    
+    // let (x_batch, y_batch) = get_batch(charidxs, batch_size, block_size);
+    // println!("x = {:?}", x_batch);
+    // println!("y = {:?}", y_batch);
 
-    let mut transformer = Transformer::new(vocab_size,n_embd,block_size,n_layers,n_heads,head_size);
-    let (logits_batch,loss) = transformer.forward(&x_batch,Some(&y_batch)); // (B,T,vocab_size)
-    println!("shape of logits: {:?}", (logits_batch.len(),logits_batch[0].len(),logits_batch[0][0].len()));
+    let mut transformer =
+        Transformer::new(vocab_size, n_embd, block_size, n_layers, n_heads, head_size);
 
-    // drop batch paralleilisation
-    for (logits,y) in logits_batch.iter().zip(y_batch.iter()) {
-        let probs = nn::utils::softmax(&logits, 1); // (T,vocab_size)
+    // let (logits_batch, loss) = transformer.forward(&x_batch, Some(&y_batch)); // (B,T,vocab_size)
+    // println!(
+    //     "shape of logits: {:?}",
+    //     (
+    //         logits_batch.len(),
+    //         logits_batch[0].len(),
+    //         logits_batch[0][0].len()
+    //     )
+    // );
 
-    }
-    
-    
-    // let new_idxs = transformer.generate([0].to_vec(), 100);
-
-    // let mut x_str = String::new();
-    // for idx in new_idxs {
-    //     x_str.push(itochar.get(&idx).unwrap().to_owned());
+    // // drop batch paralleilisation
+    // for (logits, y) in logits_batch.iter().zip(y_batch.iter()) {
+    //     let probs = nn::utils::softmax(&logits, 1); // (T,vocab_size)
     // }
-    // println!("{:?}",x_str);
+
+    let new_idxs = transformer.generate([0].to_vec(), 100);
+
+    let mut x_str = String::new();
+    for idx in new_idxs {
+        x_str.push(itochar.get(&idx).unwrap().to_owned());
+    }
+    println!("{}", x_str);
 }
